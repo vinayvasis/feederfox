4f10a13d5b9312d3b9cbec2d3e5d83402719c9a8
vinay <vinaytm7@gmail.com>
2018-07-22 14:32:05 +0530

    initial commit

--- FeederFoxnew/settings.py
+++ FeederFoxnew/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'bggmih8qx-!e*z!!8qg&a&=r9(k&w-c^x@a(bysh!-vy3ar-a2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['feederfox.herokuapp.com']
 
 
 # Application definition
