e1c2c4f62438a4693d420ec2646a1cb30e1c3072
vinayvasis <vinaytm7@gmail.com>
2018-07-22 16:04:17 +0530

    Update settings.py

--- FeederFoxnew/settings.py
+++ FeederFoxnew/settings.py
@@ -136,8 +136,3 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
